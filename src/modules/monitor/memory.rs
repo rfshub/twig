9cd74d7ffd70f5564e16d187bbc0fc2b2f3e1851
Canmi <9997200@qq.com>
2025-07-28 15:07:08 +0800

    fix(mac): ram & swap status

--- src/modules/monitor/memory.rs
+++ src/modules/monitor/memory.rs
@@ -4,7 +4,6 @@ use crate::core::response;
 use axum::response::Response;
 use serde::Serialize;
 use serde_json::json;
-use sysinfo::System;
 
 #[derive(Serialize)]
 struct MemoryInfo {
@@ -15,18 +14,62 @@ struct MemoryInfo {
     unit: &'static str,
 }
 
-// Handles requests for memory status.
-pub async fn get_memory_handler() -> Response {
-    let mut sys = System::new_all();
-    sys.refresh_memory();
+#[cfg(target_os = "macos")]
+mod platform {
+    use super::*;
+    use crate::modules::macmon::fetch::fetch_macmon;
 
-    let mem_info = MemoryInfo {
-        total: sys.total_memory(),
-        used: sys.used_memory(),
-        total_swap: sys.total_swap(),
-        used_swap: sys.used_swap(),
-        unit: "bytes",
-    };
+    pub async fn get_memory_handler() -> Response {
+        if let Some(data) = fetch_macmon().await {
+            if let Some(mem) = data.get("memory") {
+                let total = mem.get("ram_total").and_then(|v| v.as_u64()).unwrap_or(0);
+                let used = mem.get("ram_usage").and_then(|v| v.as_u64()).unwrap_or(0);
+                let total_swap = mem.get("swap_total").and_then(|v| v.as_u64()).unwrap_or(0);
+                let used_swap = mem.get("swap_usage").and_then(|v| v.as_u64()).unwrap_or(0);
+                let mem_info = MemoryInfo {
+                    total,
+                    used,
+                    total_swap,
+                    used_swap,
+                    unit: "bytes",
+                };
 
-    response::success(Some(json!(mem_info)))
+                return response::success(Some(json!(mem_info)));
+            }
+        }
+
+        // null
+        let mem_info = MemoryInfo {
+            total: 0,
+            used: 0,
+            total_swap: 0,
+            used_swap: 0,
+            unit: "bytes",
+        };
+
+        response::success(Some(json!(mem_info)))
+    }
 }
+
+#[cfg(target_os = "linux")]
+mod platform {
+    use super::*;
+    use sysinfo::{System};
+
+    pub async fn get_memory_handler() -> Response {
+        let mut sys = System::new_all();
+        sys.refresh_memory();
+
+        let mem_info = MemoryInfo {
+            total: sys.total_memory(),
+            used: sys.used_memory(),
+            total_swap: sys.total_swap(),
+            used_swap: sys.used_swap(),
+            unit: "bytes",
+        };
+
+        response::success(Some(json!(mem_info)))
+    }
+}
+
+pub use platform::get_memory_handler;
