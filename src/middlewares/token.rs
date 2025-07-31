b77a3c5787484189e864f248484a1deb640e0f96
Canmi <9997200@qq.com>
2025-07-31 15:16:35 +0800

    def: execpt token

--- src/middlewares/token.rs
+++ src/middlewares/token.rs
@@ -18,7 +18,7 @@ pub async fn handler(req: Request<Body>, next: Next) -> Response {
 
     let stage = CONFIG.stage.to_lowercase();
     if stage == "development" || stage == "dev" {
-        log::log(log::LogLevel::Debug, "▪ skip auth");
+        log::log(log::LogLevel::Debug, "! skip auth");
         return next.run(req).await;
     }
 
@@ -26,12 +26,15 @@ pub async fn handler(req: Request<Body>, next: Next) -> Response {
     let header_str = raw_header.and_then(|v| v.to_str().ok());
 
     if raw_header.is_none() {
-        log::log(log::LogLevel::Debug, "▪ 403: no authorization header");
+        log::log(log::LogLevel::Debug, "! 403: no authorization header");
         return response::forbidden();
     }
 
     if header_str.is_none() || !header_str.unwrap().starts_with("Bearer ") {
-        log::log(log::LogLevel::Debug, &format!("▪ 403: invalid header format: {:?}", header_str));
+        log::log(
+            log::LogLevel::Debug,
+            &format!("! 403: invalid header format: {:?}", header_str),
+        );
         return response::forbidden();
     }
 
@@ -41,7 +44,14 @@ pub async fn handler(req: Request<Body>, next: Next) -> Response {
     if tokens.iter().any(|valid| token == valid) {
         next.run(req).await
     } else {
-        log::log(log::LogLevel::Debug, &format!("▪ 403: token mismatch, received: {}", token));
+        log::log(
+            log::LogLevel::Debug,
+            &format!(
+                "! 403: token mismatch, received: {}, expected: {:?}",
+                token,
+                tokens
+            ),
+        );
         response::forbidden()
     }
 }
